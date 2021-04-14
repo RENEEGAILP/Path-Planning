1de5c2112ec7d080e100ae5fb979bfd8a987207e
RENEEGAILP <22723224+RENEEGAILP@users.noreply.github.com>
2021-04-14 11:49:05 -0400

    Modifying the rrtstar file and moving moveToGoal to test_env.py. Added code for resetting camera position

--- PathPlanning/src/rrtstar_simulator.py
+++ PathPlanning/src/rrtstar_simulator.py
@@ -3,15 +3,16 @@ import numpy as np
 
 # creating the environment
 p.connect(p.GUI)
+
 p.loadURDF("../data/plane.urdf")
 p.setGravity(0, 0, -10)
 
 # adding static obstacles to the environment (uncomment for obstacles)
-# obs_1 = p.loadURDF("../data/obstacle.urdf", [0, 10, 0], useFixedBase=1)
-# obs_2 = p.loadURDF("../data/obstacle.urdf", [-5, -10, 0], useFixedBase=1)
-# obs_3 = p.loadURDF("../data/obstacle.urdf", [5, -10, 0], useFixedBase=1)
-# obs_4 = p.loadURDF("../data/obstacle.urdf", [-5, 0, 0], useFixedBase=1)
-# obs_5 = p.loadURDF("../data/obstacle.urdf", [5, 0, 0], useFixedBase=1)
+obs_1 = p.loadURDF("../data/obstacle.urdf", [0, 10, 0], useFixedBase=1)
+obs_2 = p.loadURDF("../data/obstacle.urdf", [-5, -10, 0], useFixedBase=1)
+obs_3 = p.loadURDF("../data/obstacle.urdf", [5, -10, 0], useFixedBase=1)
+obs_4 = p.loadURDF("../data/obstacle.urdf", [-5, 0, 0], useFixedBase=1)
+obs_5 = p.loadURDF("../data/obstacle.urdf", [5, 0, 0], useFixedBase=1)
 
 # initializing the robot
 husky1pos = [1, 1, 0.1]
@@ -19,91 +20,8 @@ husky2pos = [-1, -1, 0.1]
 husky_1 = p.loadURDF("../data/husky/husky.urdf", husky1pos)
 husky_2 = p.loadURDF("../data/husky/husky.urdf", husky2pos)
 
-
-# take robot_1 to desired target location
-# referred Kinodynamic...have to figure out which joints we need to move
-def moveRobotToGoal(robot_id, target_location):
-    print("Hello")
-
-    robot_pos, robot_orn =p.getBasePositionAndOrientation(robot_id)
-    euler_orn =p.getEulerFromQuaternion(robot_orn)
-    #  pose = [cubePos[0], cubePos[1], cubeOrn[2], 0.0, 0.0]
-
-    # Controller parameters
-    Kp = 0.7  # 1.5
-
-    # get the goal location
-    xd = target_location[0]
-    yd = target_location[1]
-
-    # get the current robot location
-    xa = robot_pos[0]
-    ya = robot_pos[1]
-    thetaa = euler_orn[2]
-
-    # determine how far to rotate to face the goal point
-    # PS. ALL ANGLES ARE IN RADIANS
-    dt = (np.arctan2((yd - ya), (xd - xa))) - thetaa
-    # restrict angle to (-pi,pi)
-    dt = ((dt + np.pi) % (2.0 * np.pi)) - np.pi
-    dt = ((dt * 180.0) / np.pi)
-
-    # control input for angular velocity
-    W = (Kp * dt)
-
-    # find distance to goal
-    d = np.sqrt(pow((xd - xa), 2) + pow((yd - ya), 2))
-
-    # control input for linear velocity
-    if d > 0.05:
-      V = 10.0
-    else:
-      V = 0.0
-
-    # Parameters
-    R = 0.7  # radius of wheels
-    L = 1  # total distance between wheels
-
-    # Compute desired vel_r, vel_l needed to ensure w
-    Vr = ((2.0 * V) + (W * L)) / (2 * R)
-    Vl = ((2.0 * V) - (W * L)) / (2 * R)
-
-    # Set theta1
-    p.setJointMotorControl2(robot_id, 0, p.POSITION_CONTROL, targetPosition=0.0, force=750)
-
-    # Set theta2
-    p.setJointMotorControl2(robot_id, 1, p.POSITION_CONTROL, targetPosition=0.0, force=750)
-
-    for i in range(100):
-        p.setJointMotorControl2(robot_id, 2, p.VELOCITY_CONTROL, targetVelocity=-Vr, force=500)
-        p.setJointMotorControl2(robot_id, 3, p.VELOCITY_CONTROL, targetVelocity=-Vl, force=500)
-        p.setJointMotorControl2(robot_id, 4, p.VELOCITY_CONTROL, targetVelocity=-Vr, force=500)
-        p.setJointMotorControl2(robot_id, 5, p.VELOCITY_CONTROL, targetVelocity=-Vl, force=500)
-        p.stepSimulation()
-    return
-
-pos, orn = p.getBasePositionAndOrientation(husky_1)
-moveRobotToGoal(husky_1, [10, 10])
-
-# p.resetBasePositionAndOrientation(husky_1, [10, 10, 0], [0, 0, 1, 1])
-#
-# numJoints = p.getNumJoints(husky_1)
-# for joint in range(numJoints):
-#   print(p.getJointInfo(husky_1, joint))
-# targetVel = 1  #rad/s
-# maxForce = 100  #Newton
-#
-# for joint in range(2, 6):
-#   p.setJointMotorControl2(husky_1, joint, p.POSITION_CONTROL, targetPosition=100.0, force=maxForce)
-#   p.setJointMotorControl(husky_1, joint, p.VELOCITY_CONTROL, targetVel, maxForce)
-#   #p.setJointMotorControl(husky_2, joint, p.VELOCITY_CONTROL, -targetVel, maxForce)
-#
-# while(True):
-#   p.stepSimulation()
-#
-#
-# exit(0)
-# ------------------------------------------------------
+# Adjust the position of the camera
+p.resetDebugVisualizerCamera(cameraDistance=25, cameraYaw=-180, cameraPitch=-120, cameraTargetPosition=[0, 0, 0])
 
 numJoints = p.getNumJoints(husky_1)
 for joint in range(numJoints):
@@ -131,5 +49,3 @@ while (True):
 
 p.getContactPoints(husky_1)
 p.disconnect()
-
-
